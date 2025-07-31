f2de8f4cbc417e79d1305c5de50ac7beeec073ca
Abdulmalik <abdulmalik-abdullah@hotmail.com>
2025-08-01 00:28:58 +0300

    add java code

---
+++ src/main/java/com/company/app/App.java
@@ -0,0 +1,9 @@
+package com.company.app;
+import java.lang.reflect.Method;
+
+public class App {
+    public static void main(String[] args) {
+
+        System.out.println("Design Patterns Examples in Java");
+    }
+}
